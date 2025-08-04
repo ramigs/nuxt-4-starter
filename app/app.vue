836815b19090bd83218d4e0ed0d0ad68140a1351
ramigs <ramigs@gmail.com>
2025-08-04 12:33:17 +0100

    configure eslint

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hello from Nuxt 4!</h1>
+    <button @click="greet('Foo')">
+      Greet 1
+    </button> <!-- All good -->
+    <button @click="greet()">
+      Greet 2
+    </button> <!-- Should fail -->
   </div>
 </template>
+
+<script setup lang="ts">
+const x = 'oi!'
+
+type Ola = 1 | 2
+
+const y: Ola = 3
+
+function greet(personName: string) {
+  console.log(`Hi ${personName}!`)
+}
+</script>
+
+<style scoped></style>
